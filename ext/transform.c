1e9b980c1644d9480ed7e3d11b1eac12be5077df
Grigoriy Chudnov <g.chudnov@gmail.com>
2014-01-27 23:34:02 +0400

    orientation detector

--- ext/transform.c
+++ ext/transform.c
@@ -2,6 +2,8 @@
 #include "callback.h"
 #include "details.h"
 #include <pthread.h>
+#include <exif-data.h>
+#include <sys/stat.h>
 
 
 ID av_t_id_load;
@@ -15,16 +17,60 @@ static void av_get_image_file_size(const char* file_path, long long* file_size)
     long long sz = 0;
     if(file_path)
     {
-        struct stat sb;
-        memset(&sb, 0, sizeof(stat));
-        int err = stat(file_path, &sb);
+        int err;
+        struct stat st;
+        memset(&st, 0, sizeof(st));
+        err = stat(file_path, &st);
         if(!err)
         {
-            sz = sb.st_size;
+            sz = st.st_size;
         }
     }
 
     *file_size = sz;
+}
+
+/*
+Get image orientation from EXIF
+
+     0th Row      0th Column
+     1  top          left side
+     2  top          right side
+     3  bottom       right side
+     4  bottom       left side
+     5  left side    top
+     6  right side   top
+     7  right side   bottom
+     8  left side    bottom
+*/
+int av_get_orientation(VipsImage* image)
+{
+    int orientation = 0;
+
+    GType exif_type = vips_image_get_typeof(image, VIPS_META_EXIF_NAME);
+    if(exif_type != 0)
+    {
+        unsigned char *data;
+        size_t data_length;
+        if(!vips_image_get_blob(image, VIPS_META_EXIF_NAME, (void *)&data, &data_length))
+        {
+            ExifData *ed;
+            ed = exif_data_new_from_data(data, data_length);
+            if(ed != NULL)
+            {
+                ExifByteOrder bo = exif_data_get_byte_order(ed);
+                ExifEntry *ee = exif_data_get_entry(ed, EXIF_TAG_ORIENTATION);
+                if(ee)
+                {
+                    orientation = exif_get_short(ee->data, bo);
+                }
+
+                exif_data_free(ed);
+            }
+        }
+    }
+
+    return orientation;
 }
 
 /* Shrinks image using the provided width and height ratios */
@@ -58,17 +104,30 @@ void* av_build_image_thread_func(void* data)
     image = vips_image_new_mode(tdata->src_path, "r");
     if(image)
     {
+        // ORIENTATION
+        int orientation = av_get_orientation(image);
+        //fprintf(stderr, "orientation: %d\n", orientation);
+        if(orientation > 1)
+        {
+            // Need to transform an image so that orientation becomes 1
+        }
+
         // SHRINK
         if(tdata->dst_path && tdata->target_width && tdata->target_height)
         {
             double width_ratio = 0, height_ratio = 0;
 
-            av_get_scale_transform2(image->Xsize, image->Ysize, tdata->target_width, tdata->target_height, image_scale_fit_no_scale_up, &width_ratio, &height_ratio);
+            av_get_scale_transform2(image->Xsize, image->Ysize,
+                                    tdata->target_width, tdata->target_height,
+                                    image_scale_fit_no_scale_up,
+                                    &width_ratio, &height_ratio);
 
-            VipsImage* t = av_internal_shrink_image(image, width_ratio, height_ratio, &tdata->err_str);
+            VipsImage* t;
+            t = av_internal_shrink_image(image, width_ratio, height_ratio, &tdata->err_str);
             if(t)
             {
-                VipsImage* out = vips_image_new_mode(tdata->dst_path, "w");
+                VipsImage* out;
+                out = vips_image_new_mode(tdata->dst_path, "w");
                 if(out)
                 {
                     if(im_copy(t, out))
@@ -101,7 +160,7 @@ void* av_build_image_thread_func(void* data)
         im_close(image);
     }
 
-    av_add_to_event_qeueue(tdata);
+    av_add_to_event_queue(tdata);
 
     return NULL;
 }
